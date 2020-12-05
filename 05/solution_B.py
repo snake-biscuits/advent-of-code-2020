05de731e33148c8a331236984fb0eb6ce43ba061
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2020-12-05 16:58:27 +1000

    fitting day 5's python part B into under 280 characters

--- 05/solution_B.py
+++ 05/solution_B.py
@@ -1,7 +1,3 @@
 with open("boarding_passes.txt") as file:
     all_seats = [int(f"0b{b.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')}", 2) for b in file]
-    start, end = min(all_seats), max(all_seats)
-
-    my_seat = list(set(range(start, end)).difference(set(all_seats)))[0]
-
-    print(f"My Seat ID is {my_seat} (Row {my_seat // 8}, {my_seat % 8})")
+    print(f"My Seat ID is {set(range(min(all_seats), max(all_seats))).difference(set(all_seats))}")
