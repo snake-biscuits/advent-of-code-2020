1fb39aacee1503083351061ad3cc31f7301673eb
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2020-12-06 10:12:48 +1000

    renaming variables for day 2 part A

--- 02/solution_A.py
+++ 02/solution_A.py
@@ -1,12 +1,12 @@
 with open("passwords.txt") as file:
     inputs = [p.rstrip("\n") for p in file.readlines()]
 
-invalid_passwords = list()
+valid_passwords = list()
 for line in inputs:
     policy, password = line.split(": ")
     min_max, char = policy.split(" ")
     min_char, max_char = [int(x) for x in min_max.split("-")]
     if min_char <= password.count(char) <= max_char:
-        invalid_passwords.append(password)
+        valid_passwords.append(password)
 
-print(f"Found {len(invalid_passwords)} invalid passwords")  # 393
+print(f"Found {len(valid_passwords)} valid passwords")  # 393
