ad3b397bebd72360bbd5456513f35afcad08782b
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2020-12-09 09:07:17 +1000

    made day 5 solutions even shorter

--- 05/solution_A.py
+++ 05/solution_A.py
@@ -1,2 +1,2 @@
 with open("boarding_passes.txt") as file:
-    print(max([int(f"0b{b.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')}", 2) for b in file]))  # 913
+    print(max([int(f"0b{b.translate(dict(zip(map(ord, 'FBLR'), '0101')))}", 2) for b in file]))  # 913
