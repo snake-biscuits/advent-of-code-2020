14b920443c4930e369d9be6a330170f47b28cf82
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2020-12-10 08:21:01 +1000

    rethinking day 8 part 2

--- 08/solution_B.py
+++ 08/solution_B.py
@@ -1,42 +1,32 @@
 with open("instructions.txt") as file:
     instructions = [(f, int(a)) for f, a in (i.split() for i in file)]
 
-program_counter = 0
-flipped_instructions = set()
-while program_counter < len(instructions) - 1:
-    accumulator = 0
-    program_counter = 0
-    executed_lines = set()
-    flipped = False  # flip once per run
-    while program_counter not in executed_lines:
-        function, argument = instructions[program_counter]
-        executed_lines.add(program_counter)
-        if function == "acc":
-            accumulator += argument
-            program_counter += 1
-        elif function == "jmp":
-            if not flipped:
-                if program_counter not in flipped_instructions:
-                    print("nop", argument)
-                    flipped = True
-                    flipped_instructions.add(program_counter)
-##                    print(f"flipped {program_counter}")
-                    pass  # treat as nop
-            program_counter += argument
-        elif function == "nop":
-            if not flipped:
-                if program_counter not in flipped_instructions:
-                    if argument == 0:  # skip nop 0
-                        flipped_instructions.add(program_counter)
-                    else:
-                        print("jmp", argument)
-                        flipped = True
-                        flipped_instructions.add(program_counter)
-##                        print(f"flipped {program_counter}")
-                        program_counter += argument - 1  # treat as jmp
-            program_counter += 1
-    print(program_counter)
-##    print(flipped_instructions)
-    print("-" * 30)
 
-print(f"accumulator value is {accumulator}")
+def run_asm(code):
+    acc = 0
+    executed = set()
+    pc = 0
+    while pc not in executed:
+        func, arg = code[pc]
+        if func == "acc":
+            acc += arg
+            pc += 1
+        elif func == "jmp":
+            yield pc
+            pc += arg
+        elif func == "nop":
+            yield pc
+            pc += 1
+
+        if pc == len(code):
+            print(f"Successfully Executed. {acc = }")
+            return
+    return  # run failed
+
+
+flipped_instructions = set()
+for flippable in run_asm(instructions):  # the yielded
+    modded_code = instructions
+    flipped = (??? if f else ???, a for f, a in modded_code[flippable])
+    modded_code[flippable] = flipped
+    run = list(run_asm(modded_code))
