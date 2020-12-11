a180c5b39b6dd74a77f02b4da067b97e9d78dba2
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2020-12-11 11:52:48 +1000

    very stumped

--- 10/solution_B.py
+++ 10/solution_B.py
@@ -1,32 +1,18 @@
+import math
+
+
 with open("adapters.txt") as file:
     adapters = sorted([int(i) for i in file])
 
-skippable = 1
+possible_solutions = 1
 previous = 0
 last = max(adapters) + 3
 for node, next_node in zip(adapters, [*adapters[1:], last]):
     if node - previous == 1:
-        print(node, next_node)
-        if next_node - previous <= 3:  # current node can be skipped
-            skippable += 1
+        if next_node - previous == 3:  # current node can be skipped
+            possible_solutions += 1
+        if next_node - previous < 3:
+            possible_solutions += 2
     previous = node
 
-print((skippable ** 2) + 2)  # 2027 too low
-
-# 0 to first always 3
-# last to device always 3
-
-# for 4 skippables
-# all in
-
-# 1, 2, 3, 4
-
-# 1 + 2, 1 + 3, 1 + 4
-# 2 + 3, 2 + 4
-# 3 + 4
-# ^ (4 - 1) ** 2 + (4 - 1)
-
-# 1 + 2 + 3, 1 + 2 + 4, 1 + 3 + 4
-# 2 + 3 + 4
-
-...
+# 2027 too low
