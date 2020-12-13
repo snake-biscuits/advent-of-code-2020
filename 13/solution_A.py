3b313d13d7f93019005e495319e5d5af94baba3e
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2020-12-13 18:21:03 +1000

    day 13 python solution A

---
+++ 13/solution_A.py
@@ -0,0 +1,18 @@
+import collections
+
+
+with open("buses.txt") as file:
+    earliest, bus_ids = [line.rstrip("\n") for line in file]
+
+earliest = int(earliest)
+bus_ids = [int(b) for b in bus_ids.split(",") if b != "x"]
+
+wait = collections.defaultdict(int)
+for bus_id in bus_ids:
+    length = bus_id - earliest % bus_id
+    wait[length] = bus_id
+
+shortest = min(wait)
+
+print(f"ID of earliest bus: {wait[shortest]}, wait time: {shortest}")
+print(f"Answer: {wait[shortest] * shortest}")
