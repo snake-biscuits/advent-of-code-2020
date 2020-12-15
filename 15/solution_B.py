7dc2adf2ded786c6b8c93bd5c9c519385d237f60
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2020-12-15 22:38:45 +1000

    day 16 python solution B

---
+++ 15/solution_B.py
@@ -0,0 +1,17 @@
+import collections
+
+puzzle_input = [0, 1, 4, 13, 15, 12, 16]
+
+occurences = collections.defaultdict(list)
+occurences.update({x: [i + 1] for i, x in enumerate(puzzle_input)})
+last = 6
+for i in range(len(puzzle_input) + 1, 30000000 + 1):
+    if len(occurences[last]) >= 2:
+        a, b = occurences[last][-2:]
+        x = b - a
+    else:
+        x = 0
+    occurences[x].append(i)
+    last = x
+
+print(f"The 30000000th number spoken in {x}")  # 16439
